55c4a37e34d038a7e40dd5180c65c78120d3049b
annafiguerolaruiz <91455277+annafiguerolaruiz@users.noreply.github.com>
2022-10-18 09:10:54 +0200

    Aaa

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,31 +7,58 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     public const string HORIZONTAL = "Horizontal", VERTICAL = "Vertical";
 
+    public static bool playerCreated;
+
     private float inputTol = 0.2f; // Input tolerance
     private float xInput, yInput;
 
-    // Start is called before the first frame update
-    void Start()
+    private bool isWalking;
+    private Vector2 lastDirection;
+    private Animator _animator;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
     {
-        
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        playerCreated = true;
     }
 
-    // Update is called once per frame
     void Update()
-
     {
         xInput = Input.GetAxisRaw(HORIZONTAL);
+        yInput = Input.GetAxisRaw(VERTICAL);
+        isWalking = false;
+     
         if (Mathf.Abs(xInput) > inputTol)
         {
-            Vector3 translation = new Vector3(xInput * speed * Time.deltaTime, 0, 0);
-            transform.Translate(translation);
+            _rigidbody.velocity = new Vector2(xInput * speed, 0);
+            isWalking = true;
+            lastDirection = new Vector2(xInput, 0);
         }
-        xInput = Input.GetAxisRaw(VERTICAL);
-        if (Mathf.Abs(xInput) > inputTol)
+
+        if (Mathf.Abs(yInput) > inputTol)
         {
-            Vector3 translation = new Vector3( x:0 ,xInput * speed * Time.deltaTime, z:0);
-            transform.Translate(translation);
+            _rigidbody.velocity = new Vector2(0,yInput * speed);
+            isWalking = true;
+            lastDirection = new Vector2(0, yInput);
         }
     }
 
+    private void LateUpdate()
+    {
+        if (!isWalking)
+        {
+            _rigidbody.velocity = Vector2.zero;
+        }
+        _animator.SetFloat(name: HORIZONTAL, xInput);
+        _animator.SetFloat(name: VERTICAL, yInput);
+        _animator.SetFloat(name: "LastHorizontal",lastDirection.x);
+        _animator.SetFloat(name: "LastVertical", lastDirection.y);
+        _animator.SetBool(name:"IsWalking",isWalking);
+    }
 }
